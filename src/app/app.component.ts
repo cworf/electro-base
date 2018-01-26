3db4770e22674f4fa3793c02b4898ae76723c1a3
cworf <colin.worf@gmail.com>
2018-01-26 13:47:54 -0800

    create add post method and simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'; //RxJS or Reactive Extensions for JavaScript is a library for transforming, composing, and querying streams of all kinds of data, from simple arrays of values, to series of events, to complex flows of data
 
-interface post{
+interface Post{
 	title: string;
 	content: string;
 }
+
+interface PostId
 
 @Component({
   selector: 'app-root',
@@ -18,11 +20,18 @@ export class AppComponent {
 	postsCollection: AngularFirestoreCollection<post>;
 	posts: Observable<post[]>;
 
+	title: string;
+	content: string;
 
   constructor(private afs: AngularFirestore){}
 
   ngOnInit(){
 	  this.postsCollection = this.afs.collection('posts');
 	  this.posts = this.postsCollection.valueChanges();
+  }
+
+  addPost(){
+	  this.postsCollection.add({'title': this.title, 'content': this.content})
+	  // this.postsCollection.doc('my-custom-id').set({'title': this.title, 'content': this.content}) //this is how you make a custom id
   }
 }
