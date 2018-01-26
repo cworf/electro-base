c1f5c642921dfaf04062f3a933bce8d2ec075fd5
cworf <colin.worf@gmail.com>
2018-01-26 12:38:59 -0800

    create posts collection getter on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'; //RxJS or Reactive Extensions for JavaScript is a library for transforming, composing, and querying streams of all kinds of data, from simple arrays of values, to series of events, to complex flows of data
+
+interface post{
+	title: string;
+	content: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+	postsCollection: AngularFirestoreCollection<post>;
+	posts: Observable<post[]>;
+
+
+  constructor(private afs: AngularFirestore){}
+
+  ngOnInit(){
+	  this.postsCollection = this.afs.collection('posts');
+	  this.posts = this.postsCollection.valueChanges();
+  }
 }
