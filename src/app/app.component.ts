55aea163cd641f518de42756f7d5c9e9c4dc7f4a
cworf <colin.worf@gmail.com>
2018-01-26 14:08:22 -0800

    modify post object to include post id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,9 @@ interface Post{
 	content: string;
 }
 
-interface PostId
+interface PostId extends Post{
+	id: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -17,8 +19,8 @@ interface PostId
 })
 export class AppComponent {
 
-	postsCollection: AngularFirestoreCollection<post>;
-	posts: Observable<post[]>;
+	postsCollection: AngularFirestoreCollection<Post>;
+	posts: any;
 
 	title: string;
 	content: string;
@@ -27,11 +29,20 @@ export class AppComponent {
 
   ngOnInit(){
 	  this.postsCollection = this.afs.collection('posts');
-	  this.posts = this.postsCollection.valueChanges();
+	  // this.posts = this.postsCollection.valueChanges();  //this code gets all of the values defined above
+	  this.posts = this.postsCollection.snapshotChanges()  //this code gets all values inside the firestore object and allows you to map the unique post id into the returned object
+	  	.map(actions => { //actions represents the loop through each post
+			return actions.map(a => { //a represents a loop through the parameters of each post
+				const data = a.payload.doc.data() as Post; //note that the returned post parameters(title and content) or whatever gets added down the road will be mapped into the data object
+				const id = a.payload.doc.id;
+				console.log({id, data});
+				return { id, data }; //this is the new object given to each post
+			});
+		}); console.log(this.posts)
   }
 
   addPost(){
 	  this.postsCollection.add({'title': this.title, 'content': this.content})
-	  // this.postsCollection.doc('my-custom-id').set({'title': this.title, 'content': this.content}) //this is how you make a custom id
+	  // this.postsCollection.doc('my-custom-id').set({'title': this.title, 'content': this.content}) //this is how you make a custom id if needed
   }
 }
