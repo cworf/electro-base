37cdb23bb688bfcef1241416eff072c909e6e793
cworf <colin.worf@gmail.com>
2018-01-26 11:55:49 -0800

    add angularFire2 modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 
+var firebaseConfig = {
+	apiKey: "AIzaSyAAavzU127vTBhpDUsb7ohOMQO5Y30AP3I",
+    authDomain: "electro-base.firebaseapp.com",
+    databaseURL: "https://electro-base.firebaseio.com",
+    projectId: "electro-base",
+    storageBucket: "electro-base.appspot.com",
+    messagingSenderId: "435821102298"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FormsModule,
+	AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
