9d42b1b992b5207fbaf409a34be03b2ffbeb9ad3
cworf <colin.worf@gmail.com>
2018-01-26 17:56:52 -0800

    create login service methods

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -21,11 +21,38 @@ export class AuthService {
 	user: Observable<User>;
 
   constructor(private afAuth : AngularFireAuth, private afs : AngularFirestore, private router: Router) {
-	  this.user = this.afAuth.authState.switchMap(user => {
+	  this.user = this.afAuth.authState.switchMap(user => { //will determine if a user is currently authenticated and return their user information into this.user as an observable
 		  if (user) {
 		      return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+		  } else {
+			  return Observable.of(null)
 		  }
-	  })
+	  });
   }
+
+  googleLogin() { //this function is activated when google login is selected and sends the google auth provider into the oAuth function
+    const provider = new firebase.auth.GoogleAuthProvider()
+    return this.oAuthLogin(provider);
+  }
+
+  private oAuthLogin(provider) { //this function can be used to log in with many providers by creating other login methods which send the provider into this parameter
+    return this.afAuth.auth.signInWithPopup(provider) //the successful sign in returns an object containing the user data which will be sent into "credential" parameter after it is received
+      .then((credential) => {
+        this.updateUserData(credential.user)
+      })
+  }
+
+  private updateUserData(user) {
+    // Sets user data to firestore on login
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const data: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL
+    }
+    return userRef.set(data)
+  }
+
 
 }
