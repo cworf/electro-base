da80f862fa25ad99c2b8e999f53f0c35ad7fce62
cworf <colin.worf@gmail.com>
2018-01-27 00:31:04 -0800

    add normal login method

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -30,10 +30,21 @@ export class AuthService {
 	  });
   }
 
+  normalLogin(email, pw){
+	  firebase.auth().createUserWithEmailAndPassword(email, pw)
+	  	.then((user) => {
+			this.updateUserData(user);
+		})
+	  	.catch(function(error){
+			console.log(error.message)
+		});
+  }
+
   googleLogin() { //this function is activated when google login is selected and sends the google auth provider into the oAuth function
     const provider = new firebase.auth.GoogleAuthProvider()
     return this.oAuthLogin(provider);
   }
+
 
   private oAuthLogin(provider) { //this function can be used to log in with many providers by creating other login methods which send the provider into this parameter
     return this.afAuth.auth.signInWithPopup(provider) //the successful sign in returns an object containing the user data which will be sent into "credential" parameter after it is received
@@ -49,7 +60,7 @@ export class AuthService {
     const data: User = {
       uid: user.uid,
       email: user.email,
-      displayName: user.displayName,
+      displayName: user.displayName ? user.displayName : user.email,
       photoURL: user.photoURL
     }
     return userRef.set(data)
