0dd3637a3fc480bea5cb482bde87f835e284acba
cworf <colin.worf@gmail.com>
2018-01-26 16:33:14 -0800

    add auth service interface and import required files

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import { Router } from "@angular/router";
+import * as firebase from 'firebase/app';
+
+import 'rxjs/add/operator/switchMap';
+
+interface User { //this will be the structure of the user collection
+	uid: string;
+	email: string;
+	photoURL?: string; //      )
+	displayName?: string;//    >  custom attributes have the ? mark
+	favoriteColor?: string;//  )
+}
 
 @Injectable()
 export class AuthService {
